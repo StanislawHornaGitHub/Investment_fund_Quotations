56dba9262d40b13d42b557c3d10c5a92f9bec417
Docker Scheduler <stanislawhorna@outlook.com>
2024-03-11 17:51:36 +0100

    Initial version of methodology to analyze refund rate for each product

--- Dependencies/Class_AnalizyFund.py
+++ Dependencies/Class_AnalizyFund.py
@@ -10,7 +10,7 @@
 
 .NOTES
 
-    Version:            1.0
+    Version:            1.1
     Author:             Stanisław Horna
     Mail:               stanislawhorna@outlook.com
     GitHub Repository:  https://github.com/StanislawHornaGitHub/Investment_fund_quotations
@@ -18,6 +18,9 @@
     ChangeLog:
 
     Date            Who                     What
+    2024-03-11      Stanisław Horna         getSyntheticRefundData to analyze bought funds implemented.
+                                            getLastQuotationDate to return date of last quotation received from API 
+                                            getNearestFundPrice to find nearest fund price to provided date.
 
 """
 
@@ -27,6 +30,8 @@ import json
 import csv
 from lxml.html import fromstring
 from dataclasses import dataclass, field
+import datetime
+from dateutil.parser import parse
 
 # Custom created variables modules
 from Dependencies.Variables_API import *
@@ -54,7 +59,8 @@ class AnalizyFund:
         self.Category = urlBreakdown[3]
         self.ID = urlBreakdown[4]
         self.Name = urlBreakdown[5].replace("-", " ").title()
-        self.CategoryShortCut = "".join([word[0] for word in self.Category.split("-")])
+        self.CategoryShortCut = "".join(
+            [word[0] for word in self.Category.split("-")])
 
         # Invoke method to download stats for Today: Price, Currency, LastUpdate date,
         # Change Value comparing to previous day, Change percentage comparing to previous day
@@ -62,6 +68,8 @@ class AnalizyFund:
 
         # Download historical quotation in JSON format
         self.downloadHistoricalQuotation()
+
+        return None
 
     def getFundID(self) -> str:
         return self.ID
@@ -86,8 +94,111 @@ class AnalizyFund:
             )
         except:
             priceToReturn = None
-        
+
         return priceToReturn
+
+    def getLastQuotationDate(self) -> datetime.date:
+        
+        # return date parsed to datetime type from last entry in quotation dict
+        return (
+            parse(
+                self.QuotationJSON["Price"][-1][analizyplAPIresponse_QuotationDate]
+            ).date()
+        )
+
+    def getNearestFundPrice(self, date: datetime.date, daysLimit: int = 7) -> float:
+        # init local variables to look for fund quotation
+        daysToSubtract = 0
+        newDateToCheck = date
+        priceToReturn = None
+
+        # Loop until price is None or daysToSubtract is greater than provided daysLimit
+        # It makes no sense to look further in the past for the quotation of particular fund investment
+        while priceToReturn == None and daysToSubtract <= daysLimit:
+
+            # calculate new date to check the quotation
+            newDateToCheck -= datetime.timedelta(
+                days=daysToSubtract
+            )
+
+            # get the fund price for new (older) date
+            # if it will be different than None, than while loop will not be continued
+            priceToReturn = self.getFundPriceOnDate(
+                newDateToCheck.strftime("%Y-%m-%d")
+            )
+
+            # Increment value for next iteration if value will be still None
+            daysToSubtract += 1
+
+        # return found price, 
+        # if while loop will not find the valid price, 
+        # None will be returned
+        return priceToReturn
+
+    def getSyntheticRefundData(self, paymentPeriods: dict[str, any]) -> dict[str, float]:
+
+        # loop through payments
+        for i in range(0, len(paymentPeriods)):
+
+            # get local var for better readability
+            startDate = paymentPeriods[i]["startDate"]
+            endDate = paymentPeriods[i]["endDate"]
+
+            # get prices for the beginning and end of the period.
+            startPrice = self.getNearestFundPrice(startDate)
+            endPrice = self.getNearestFundPrice(endDate)
+
+            # calculate the duration for current amount of money
+            # -1, because money can produce first profit the day after the buy date
+            paymentPeriods[i]["timeFrameInDays"] = (
+                endDate - startDate
+            ).days - 1
+
+            # calculate the refund
+            # -1 to get the profit or loss only
+            # * 100 to convert it to the %
+            paymentPeriods[i]["refund"] = (
+                (endPrice / startPrice) - 1
+            ) * 100
+
+        # number of days during which money can make profit
+        numOfDays = sum(
+            [
+                item["timeFrameInDays"]
+                for item in paymentPeriods
+            ]
+        )
+
+        # calculate weighted average for the fund,
+        fundRefund = (
+            # value <- refund for particular period
+            # timeFrame <- the weight of the value
+            sum(
+                [
+                    item["timeFrameInDays"] * item["refund"]
+                    for item in paymentPeriods
+                ]
+            )
+            /  # divided
+            # sum of the days taken under the consideration
+            numOfDays
+        )
+
+        # calculate the refund per day
+        fundRefundPerDay = (
+            fundRefund
+            /
+            numOfDays
+        )
+
+        # return predefined dict
+        return {
+            "FundName": self.Name,
+            "FundID": self.ID,
+            "RefundPercentage": fundRefund,
+            "RefundPerDayPercentage": fundRefundPerDay,
+            "RefundYearly": fundRefundPerDay * 365
+        }
 
     def downloadLatestDetails(self):
 
@@ -100,7 +211,8 @@ class AnalizyFund:
         # Get price value filtered by `xpathFilter_Price`
         # .strip() <- to replace spaces, new lines, etc
         # .replace(",", ".") <- to change decimal separator to `.`
-        self.Price = str(treeHTML.xpath(xpathFilter_Price)[0]).strip().replace(",", ".")
+        self.Price = str(treeHTML.xpath(xpathFilter_Price)
+                         [0]).strip().replace(",", ".")
 
         # Get currency value filtered by `xpathFilter_Price`
         # .strip() <- to replace spaces, new lines, etc
@@ -110,14 +222,16 @@ class AnalizyFund:
         # Get last update date value filtered by `xpathFilter_Price`
         # .strip() <- to replace spaces, new lines, etc
         # .replace(",", ".") <- to change decimal separator to `.`
-        self.UpdateDate = str(treeHTML.xpath(xpathFilter_UpdateDate)[0]).strip()
+        self.UpdateDate = str(treeHTML.xpath(
+            xpathFilter_UpdateDate)[0]).strip()
 
         # Get change percentage value filtered by `xpathFilter_Price`
         # .strip() <- to replace spaces, new lines, etc
         # .replace("/", "")[:-1] <- remove / which is in the same field and skip % sign
         # .replace(",", ".") <- to change decimal separator to `.`
         self.ChangePercentage1D = (
-            str(treeHTML.xpath(xpathFilter_ChangePercentage1D)[1].replace("/", "")[:-1])
+            str(treeHTML.xpath(xpathFilter_ChangePercentage1D)
+                [1].replace("/", "")[:-1])
             .strip()
             .replace(",", ".")
         )
@@ -126,7 +240,8 @@ class AnalizyFund:
         # .strip() <- to replace spaces, new lines, etc
         # .replace(",", ".") <- to change decimal separator to `.`
         self.ChangeValue1D = (
-            str(treeHTML.xpath(xpathFilter_ChangeValue1D)[0]).strip().replace(",", ".")
+            str(treeHTML.xpath(xpathFilter_ChangeValue1D)
+                [0]).strip().replace(",", ".")
         )
         return None
 
